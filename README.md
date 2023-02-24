76a57478b6b703bade265eca92270773f587bf1b
Martin Kavík <martin@kavik.cz>
2023-02-24 15:05:46 +0100

    complete test scenario with insert

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # sqlx-memory-debug
+
+A test app to demonstrate a memory leak / surprising unlimited caching.
+
+1. `Postgres` 13.5 or newer
+2. Update `.env`
+3. Create the table `test_table` with one column with the type `bytea`
+4. `cargo run (-r)`
+5. Open a system process monitor and notice memory usage of the process `sqlx-memory-debug`
+6. Press `ctrl+c` to close the pool
+7. Notice memory usage again
+8. Press `ctrl+c` again to stop the program
+
+![Screenshot](screenshot.png)
--- src/main.rs
+++ src/main.rs
@@ -3,19 +3,29 @@ async fn main() -> Result<(), sqlx::Error> {
     dotenvy::dotenv().unwrap();
 
     let pool = sqlx::postgres::PgPoolOptions::new()
-        .max_connections(5)
         .connect(&dotenvy::var("DATABASE_URL").unwrap()).await?;
 
-    let data = b"a new ticket";
+    let data: Vec<u8> = vec![1;100_000_000];
+    println!("test data size: {} MB", std::mem::size_of_val(data.as_slice()) as f32 / 1_000_000.);
 
     let row = sqlx::query!("INSERT INTO test_table VALUES ($1)", data)
 		.execute(&pool)
 		.await?;
-
     println!("{row:#?}");
+
+    drop(data);
+
+    println!("press ctrl+c to delete data in the table and close the pool");
+    tokio::signal::ctrl_c().await?;
+
+    let row = sqlx::query!("DELETE FROM test_table")
+        .execute(&pool)
+        .await?;
+    println!("\n{row:#?}");
+
+    pool.close().await;
 
     println!("press ctrl+c to stop the program");
     tokio::signal::ctrl_c().await?;
-    println!("\nctrl-c received!");
     Ok(())
 }
