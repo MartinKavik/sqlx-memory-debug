3357da642ef05dfbd26bd3c55b0070c25ac01fb2
Martin Kavík <martin@kavik.cz>
2023-02-24 12:14:32 +0100

    basic sqlx example

---
+++ src/main.rs
@@ -0,0 +1,16 @@
+use sqlx::postgres::PgPoolOptions;
+
+#[tokio::main]
+async fn main() -> Result<(), sqlx::Error> {
+    let pool = PgPoolOptions::new()
+        .max_connections(5)
+        .connect("postgres://postgres:postgres@localhost/sqlx-memory-debug").await?;
+
+    let row: (i64,) = sqlx::query_as("SELECT $1")
+        .bind(150_i64)
+        .fetch_one(&pool).await?;
+
+    assert_eq!(row.0, 150);
+
+    Ok(())
+}
